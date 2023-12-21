33b713afc73241968ccb51a5b4ac8c47d9e0b32f
Septiancesare <112379597+Septiancesare@users.noreply.github.com>
2023-12-21 21:15:02 +0700

    about-us

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,19 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", checkScroll);
   });
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const listItems = document.querySelectorAll(".komitmen-layanan-komunitas ul li");
+    const sections = document.querySelectorAll(".komitmen-layanan-komunitas .komitmen, .komitmen-layanan-komunitas .Community, .komitmen-layanan-komunitas .Service");
+  
+    listItems.forEach((item, index) => {
+      item.addEventListener("click", function () {
+        // Hapus kelas 'active' dari semua elemen
+        listItems.forEach((li) => li.classList.remove("active"));
+        sections.forEach((section) => section.classList.remove("active"));
+  
+        // Tambahkan kelas 'active' ke elemen yang diklik
+        item.classList.add("active");
+        sections[index].classList.add("active");
+      });
+    });
+  });
