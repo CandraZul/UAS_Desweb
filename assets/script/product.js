32667acdc3dd71e2895ef55b27a3dd778bf8b986
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2023-12-22 14:01:30 +0700

    revisi product

--- assets/script/product.js
+++ assets/script/product.js
@@ -8,20 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const selectedCategory = this.getAttribute('data-category');
 
-            // Remove the 'active' class from all categories
             categories.forEach(c => c.classList.remove('active'));
-            // Add the 'active' class to the clicked category
+
             this.classList.add('active');
 
-            // Hide all products
+
             products.forEach(product => {
                 product.style.display = 'none';
             });
 
-            // Show products with the selected category
             const filteredProducts = document.querySelectorAll(`.card.${selectedCategory}`);
             filteredProducts.forEach((product, index) => {
-                // Set animation delay dynamically
+                
                 product.style.animationDelay = `${0.5 * (index + 1)}s`;
                 product.style.display = 'block';
             });
